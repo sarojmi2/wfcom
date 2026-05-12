09b949f6f113076bb509e15a7a34e2291257465d
sarojmi2 <saroj_mi2@yahoo.co.in>
2026-05-12 17:19:48 +0530

    externa, link

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -13,5 +13,30 @@ export default function decorate(block) {
     ul.append(li);
   });
   ul.querySelectorAll('picture > img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
+
+  // Open all links in a new tab
+  ul.querySelectorAll('a').forEach((a) => {
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+  });
+
+  // Wrap card image in the first link found in the card body
+  ul.querySelectorAll('li').forEach((li) => {
+    const imageDiv = li.querySelector('.cards-card-image');
+    const firstLink = li.querySelector('.cards-card-body a');
+    if (imageDiv && firstLink) {
+      const picture = imageDiv.querySelector('picture');
+      if (picture) {
+        const imgLink = document.createElement('a');
+        imgLink.href = firstLink.href;
+        imgLink.target = '_blank';
+        imgLink.rel = 'noopener noreferrer';
+        imgLink.setAttribute('aria-label', firstLink.textContent.trim() || 'Card link');
+        picture.replaceWith(imgLink);
+        imgLink.append(picture);
+      }
+    }
+  });
+
   block.replaceChildren(ul);
 }
